eb9551c2652623d6e45db7c74ac55aa5c32ec9eb
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2020-12-18 21:56:55 +0900

    add teleop joy node

---
+++ scripts/teleop_joy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+max_vel_x = 0.5
+max_vel_y = 0.5
+max_vel_th = 1.0
+
+
+def handle_joy(self, joy_msg):
+    twist = Twist()
+    twist.linear.x = joy_msg.axes[0] * max_vel_x
+    twist.linear.y = joy_msg.axes[1] * max_vel_y
+    twist.angular.z = joy_msg.axes[0] * max_vel_th
+    pub.publish(twist)
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('teleop_joy')
+        sub = rospy.Subscriber('joy', Joy, handle_joy, queue_size=16)
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
